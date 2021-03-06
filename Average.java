95e0a97441e73ec9ef9588fb6b98e2793a903699
Saheel1410 <80039754+Saheel1410@users.noreply.github.com>
2021-03-06 00:15:36 -0800

    week1 assessment

---
+++ Average.java
@@ -0,0 +1,13 @@
+package assesment_01;
+
+public class Average {
+
+	public static void main(String[] args) {
+	int [] array = {1,3,4,5,6,7,8,9,12,14};
+	for (int i = 0; i < array.length; i++) {	
+	}
+	int average = 
+	
+	}
+
+}
---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+package assesment_01;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		for (int i = 24; i<289; i= i+24) {
+			System.out.println(i);
+		}
+
+	}
+
+}
