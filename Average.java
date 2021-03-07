47ea8c792fd6e37fa6bd693b49c9703adbbfa0c3
Saheel1410 <80039754+Saheel1410@users.noreply.github.com>
2021-03-06 22:23:36 -0800

    program corrected

--- Average.java
+++ Average.java
@@ -3,10 +3,14 @@ package assesment_01;
 public class Average {
 
 	public static void main(String[] args) {
-	int [] array = {1,3,4,5,6,7,8,9,12,14};
+	double [] array = {2,6,4,7,8,9,11,34,55,13};
+	double total = 0;
 	for (int i = 0; i < array.length; i++) {	
+		total = total + array[i];
+		System.out.println(total);
 	}
-	int average = 
+	double average = total / array.length;
+	System.out.println(average);
 	
 	}
 
