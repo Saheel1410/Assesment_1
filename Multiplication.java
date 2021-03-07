ee106ca20b3184d6825fcf03468c4cc8364e11ea
Saheel1410 <80039754+Saheel1410@users.noreply.github.com>
2021-03-06 22:25:15 -0800

    recoded as guided

--- Multiplication.java
+++ Multiplication.java
@@ -3,10 +3,26 @@ package assesment_01;
 public class Multiplication {
 
 	public static void main(String[] args) {
-		for (int i = 24; i<289; i= i+24) {
-			System.out.println(i);
+		int A = 24;
+		System.out.println("Table24");
+		for (int i = 1; i <=12; i++) {
+			int C = A*i;
+			System.out.println(C);
+			
 		}
-
-	}
-
+		System.out.println("Table29");
+			int B =29;
+			for (int j = 1; j <=12; j++) {
+				int D = B*j;
+				System.out.println(D);
+				
+			}
+			System.out.println("Table50");
+			int E =50;
+			for (int k = 1; k <=12; k++) {
+				int F = E*k;
+				System.out.println(F);
+				
+		}
+}
 }
